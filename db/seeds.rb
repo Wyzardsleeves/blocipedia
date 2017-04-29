ac64e374f7c5909fc022b285f2b9f09609ffb322
JayLatt <jlattimor3@gmail.com>
2017-04-28 20:48:01 -0400

    completed checkpoint-4-wikiCrud

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,56 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+require 'random_data'
+include RandomData
+
+#Create users
+5.times do
+  User.create!(
+    name: RandomData.random_name,
+    email: RandomData.random_email,
+    password: RandomData.random_sentence
+  )
+end
+users = User.all
+
+3.times do
+  Wiki.create!(
+    name:         RandomData.random_sentence,
+    description:  RandomData.random_paragraph
+  )
+end
+wikis = WIki.all
+
+=begin
+#create an admin user
+admin = User.create!(
+  name:     'Admin User',
+  email:    'admin@example.com',
+  password: 'helloworld',
+  role:     'admin'
+)
+
+#create a moderator
+moderator = User.create!(
+  name:     'Mod User',
+  email:    'moderator@examle.com',
+  password: 'helloworld',
+  role:     'moderator'
+)
+
+#create a member
+member = User.create!(
+  name:     'Member User',
+  email:    'member@example.com',
+  password: 'helloworld',
+)
+=end
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Wiki.count} topics created"
+
+#puts "#{Question.count} questions created"
